9d41a1ff0edfd31b43b175d39a8aad6c41c03cb6
Gavin.Kimpson <gkimpson@gmail.com>
2017-07-18 22:26:45 +0100

    basic todo app with remove functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,12 +6,28 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  title = 'Looping through data';
-  myArray = ['this', 'is', 'my', 'first', 'ngFor'];
+  // Variables
+  todos = [];
+  newItem = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  // Add a new Todo from the newItem variabke
+  addTodo() {
+    // Only add if the text is set
+    if (this.newItem !== '') {
+      // Push to our todos array
+      this.todos.push(this.newItem);
+
+      // Reset the variable of the input field
+      this.newItem = '';
+    }
   }
 
+  // Remove a todo by its index
+  finishTodo(index) {
+    // Remove 1 object inside our array at the given index
+    this.todos.splice(index, 1);
+  }
 
 }
